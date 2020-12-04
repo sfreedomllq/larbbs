aa6c5415b7dac7e7cd070d9ec1f013cabf0e5d55
LLQ <519229751@qq.com>
2020-12-04 19:54:13 +0800

    edit index page

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -2,5 +2,15 @@
 @section('title', 'Welcome！')
 
 @section('content')
-  <h1>随便看看吧</h1>
+  <div class="card">
+
+  不顺心的事情其实就跟你养的猫一样，每当你想正经做点事的时候，猫咪们，就开始了。
+
+  比如此时，正在踩我的键盘，咬我的笔杆。
+
+  <img src="http://gameforjoy.com/uploads/images/index/IMG20201204192611" alt="">
+
+
+  </div>
+
 @stop
