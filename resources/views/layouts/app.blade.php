1b59a4a0cc1eab25214832ea5cdc89ee86b89a35
LLQ <519229751@qq.com>
2020-12-06 11:56:56 +0800

    shut down sudo-su

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,9 +36,9 @@
     @include('layouts._footer')
   </div>
 
-  @if (app()->isLocal())
+  <!-- @if (app()->isLocal())
     @include('sudosu::user-selector')
-  @endif
+  @endif -->
 
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
